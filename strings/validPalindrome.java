c2dc8ce580488fcf69277d2e2c8e0dc532fe5e9b
sahilshivekar <sahilshivekarce@gmail.com>
2024-08-10 16:41:57 +0530

    Add solution for leetcode 125. Valid Palindrome

---
+++ strings/validPalindrome.java
@@ -0,0 +1,37 @@
+/*
+ * leetcode: 125. Valid Palindrome
+ */
+
+package strings;
+
+public class validPalindrome {
+    /*
+     * time: O(n)
+     * space: O(1)
+     */
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            right--;
+            left++;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
+    }
+}
