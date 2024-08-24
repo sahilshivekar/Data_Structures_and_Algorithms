fa871cf1ad9a22ec3e3922e3543862bf651de9a6
sahilshivekar <sahilshivekarce@gmail.com>
2024-08-24 15:06:43 +0530

    Add explaination and example for shadow copy and deep copy constructor

--- oop/constructors.java
+++ oop/constructors.java
@@ -18,49 +18,83 @@ public class constructors {
         Student s2 = new Student("Sahil");
         Student s3 = new Student(86);
         Student s4 = new Student("Sahil", 86);
+        s4.marks[0] = 97;
+        s4.marks[1] = 100;
+        s4.marks[2] = 0;
         Student s5 = new Student(68, "Shubham");
-        Student s6 = new Student(s4); //copy constructor
+        s5.marks[0] = 9;
+        s5.marks[1] = 10;
+        s5.marks[2] = 4;
+
+        Student s6 = new Student(s4); 
+        System.out.println(s6.getRollNo());
+        System.out.println(s6.getName());
+        for (int i : s6.marks) {
+            System.out.println(i);
+        }
+        s4.marks[2] = 99;
+        for (int i : s6.marks) {
+            System.out.println(i);
+        }
+        /*
+         * if you use shallow copy it will print the changed marks[2] to 99
+         * it is issue that occurs bcz java didn't made a new array of marks
+         * for s6 java just created a reference to the to that array
+         * 
+         * below in the contstructor there are two contructors written
+         * one is shallow and another is deep read it.
+         */
     }
 }
 
 class Student {
     private String name;
     private int rollNo;
+    int[] marks = new int[3];
 
     Student() {
-        System.out.println("c1");
-        System.out.println("Student is created");
+        // System.out.println("c1");
+        // System.out.println("Student is created");
     }
 
     Student(String name) {
-        System.out.println("c2");
+        // System.out.println("c2");
         this.name = name;
     }
 
     Student(int rollNo) {
-        System.out.println("c3");
+        // System.out.println("c3");
         this.rollNo = rollNo;
     }
 
     Student(String name, int rollNo) {
-        System.out.println("c4");
+        // System.out.println("c4");
         this.name = name;
         this.rollNo = rollNo;
     }
 
     Student(int rollNo, String name) {
-        System.out.println("c5");
+        // System.out.println("c5");
         this.name = name;
         this.rollNo = rollNo;
     }
 
-    //copy constructor
-    Student(Student s){
+    //! shallow copy constructor
+    // Student(Student s) {
+    //     System.out.println("c6");
+    //     this.name = s.name;
+    //     this.rollNo = s.rollNo;
+    //     this.marks = s.marks;
+    // }
+
+    //! deep copy constructor
+    Student(Student s) {
         System.out.println("c6");
         this.name = s.name;
         this.rollNo = s.rollNo;
-        System.out.println(this.name);
-        System.out.println(this.rollNo);
+        for(int i = 0; i < marks.length; i++){
+            this.marks[i] = s.marks[i];
+        }
     }
 
     void setName(String name) {
