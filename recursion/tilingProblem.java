32d42b280b5ddd8b99855b62b173a6cb005c1cf9
sahilshivekar <sahilshivekarce@gmail.com>
2024-08-27 16:54:30 +0530

    Add explainaiton for tiling problem

--- recursion/tilingProblem.java
+++ recursion/tilingProblem.java
@@ -1,22 +1,36 @@
 /*
- * this problem is not from leetcode. the problem name is "tiling problem" from the course
+ * this problem is not from leetcode. the problem name is "tiling problem" from ac
  */
 
 package recursion;
 
 public class tilingProblem {
 
-    // i got how its working but not getting why it works
+    /*
+     * keep in mind while reading belows explaination that n is decreasing by 1 or 2
+     * depending on the choice made by prev sol(n - ?)
+     * 
+     * it goes to the depth where each fun first chooses sol(n - 1) and goes to
+     * the depth and secondly chooses sol(n - 2) and goes till the end until
+     * it return 1 (it returns 1 when the second last fun chooses
+     * sol(n-1) which results in as way 1 to the problem and then the second
+     * last fun chooses sol(n-2) which results in as way 2 to the problem. this same
+     * change in choice happens with each fun and then again it goes to the depth)
+     * 
+     *
+     */
     public static int sol(int n) {
         if (n == 1 || n == 0) {
+            // second last fun choosed sol(n-1) or sol(n-2)
             return 1;
         }
 
-        // vertical
+        // choice 1
         int fnm1 = sol(n - 1);
 
-        // horizontal
+        // choice 2
         int fnm2 = sol(n - 2);
+
         return fnm1 + fnm2;
     }
 
