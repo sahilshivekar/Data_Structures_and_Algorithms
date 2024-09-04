ade63a1090bda2704002b86274bddad093c8291b
sahilshivekar <sahilshivekarce@gmail.com>
2024-09-04 20:20:21 +0530

    Add another optimal solution for leetcode: 122. Best Time to Buy and Sell Stock II

--- arrays/bestTimeToBuyAndSellStockII.java
+++ arrays/bestTimeToBuyAndSellStockII.java
@@ -20,6 +20,27 @@ public class bestTimeToBuyAndSellStockII {
         return profit;
     }
 
+    /*
+     * this solution is more optimized as its only incrementing i when the next
+     * number is lesser it saves time of assigning values to the buy variable
+     * 
+     * time: O(n)
+     * space: O(1)
+     */
+    public int maxProfit2(int[] prices) {
+        int buy = prices[0], profit = 0, sell, n = prices.length - 1, i = 0;
+        while (i < n) {
+            while (i < n && prices[i + 1] <= prices[i])
+                i++;
+            buy = prices[i];
+            while (i < n && prices[i + 1] > prices[i])
+                i++;
+            sell = prices[i];
+            profit += sell - buy;
+        }
+        return profit;
+    }
+
     public static void main(String[] args) {
         int prices[] = { 7, 1, 5, 3, 6, 4 };
         System.out.println(maxProfit(prices));
