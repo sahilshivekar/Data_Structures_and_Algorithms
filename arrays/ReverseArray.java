db202f9544cf076b28dcd85af3c101b89aca8ba7
sahilshivekar <sahilshivekarce@gmail.com>
2024-09-13 21:15:07 +0530

    comment out unused variable

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -84,7 +84,7 @@ public class ReverseArray {
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        Integer[] arr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // Integer[] arr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         // reverse3(arr2);
         // System.out.println(Arrays.toString(arr2));
         reverse4(arr, 0, arr.length - 1);
